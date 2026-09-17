98f9d9de346c623f831d8c4c9afb1976c41b63e8
ahmadzakky2022-coder <328345782+ahmadzakky2022-coder@users.noreply.github.com>
2026-09-17 16:13:20 +0000

    Use Android API 36 stable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.ahmadzakky2022.pprsw"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.ahmadzakky2022.pprsw"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
 
         versionCode = 1
         versionName = "0.1.0"
